0a45a2483b154a0bd93b372bc3ccea7f7abb7ae7
CyberFights <shadowjer011@gmail.com>
2026-07-29 18:17:39 -0400

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -48,27 +48,9 @@ async function doLogin(){
 }
 
 function logout(){
-  const s = typeof getSession === 'function' ? getSession() : null;
-  if (s && typeof socket !== 'undefined') {
-    socket.emit('forceLogout', { username: s.username });
-  }
   clearSession();
   localStorage.removeItem('currentUser');
   if (window.updateUIForSession) updateUIForSession();
   if (window.updateProfileCard) updateProfileCard(null);
   if (window.updateDMListSidebar) updateDMListSidebar();
-  if (window.updateDMBadge) updateDMBadge();
 }
-
-window.logout = logout;
-
-// Restore session on page load and re-join presence
-window.addEventListener('load', () => {
-  const user = typeof getSession === 'function' ? getSession() : null;
-  if (user && typeof socket !== 'undefined') {
-    socket.emit('login', user);
-    if (window.updateUIForSession) updateUIForSession();
-    if (window.updateProfileCard) updateProfileCard(user);
-    if (window.updateDMListSidebar) updateDMListSidebar();
-  }
-});
